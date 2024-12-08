7eb586213b954503905fe95cebb9febb9d6a72ce
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-12-08 10:44:52 +0400

    finished purchase course functionality

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -7,11 +7,13 @@ import { OrderModal } from "@/components/ui/Order";
 import { MarketHeader } from "@/components/ui/Marketplace";
 
 import { useWalletInfo } from "@/components/hooks/web3";
+import { useWeb3 } from "@/components/providers";
 
 import { getAllCourses } from "@/content/courses/fetcher";
 
 const Marketplace = ({ courses }) => {
-  const { canPurchaseCourse } = useWalletInfo();
+  const { web3, contract } = useWeb3();
+  const { canPurchaseCourse, account } = useWalletInfo();
 
   const [selectedCourse, setSelectedCourse] = useState(null);
 
@@ -19,8 +21,30 @@ const Marketplace = ({ courses }) => {
     setSelectedCourse(null);
   };
 
-  const handlePurchaseCourse = (order) => {
-    alert(JSON.stringify(order, null, 2));
+  const handlePurchaseCourse = async (order) => {
+    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id);
+    const orderHash = web3.utils.soliditySha3(
+      { type: "bytes16", value: hexCourseId },
+      { type: "address", value: account.data }
+    );
+    const emailHash = web3.utils.sha3(order.email);
+
+    const proof = web3.utils.soliditySha3(
+      { type: "bytes32", value: emailHash },
+      { type: "bytes32", value: orderHash }
+    );
+
+    const value = web3.utils.toWei(order.price.toString());
+
+    try {
+      const result = await contract.methods.purchaseCourse(hexCourseId, proof).send({
+        from: account.data,
+        value,
+      });
+      console.log(result);
+    } catch {
+      console.log("Purchase course: Operation has failed");
+    }
   };
 
   return (
