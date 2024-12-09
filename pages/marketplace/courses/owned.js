75d259039a6f29150de11521613dff5480d0d442
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-12-09 22:42:10 +0400

    style improvements

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+
 import { Button, Message } from "@/components/ui/common";
 import { OwnedCourseCard } from "@/components/ui/Course";
 import { BaseLayout } from "@/components/ui/layout";
@@ -7,6 +9,8 @@ import { useOwnedCourses, useWalletInfo } from "@/components/hooks/web3";
 import { getAllCourses } from "@/content/courses/fetcher";
 
 const OwnedCourses = ({ courses }) => {
+  const router = useRouter();
+
   const { account } = useWalletInfo();
   const { ownedCourses } = useOwnedCourses(courses, account.data);
 
@@ -16,8 +20,12 @@ const OwnedCourses = ({ courses }) => {
       <section className="grid grid-cols-1">
         {ownedCourses.data?.map((course) => (
           <OwnedCourseCard key={course.id} course={course}>
-            {/* <Message>My custom message!</Message> */}
-            <Button>Watch the course</Button>
+            {/* <Message>
+              My custom message!
+            </Message> */}
+            <Button onClick={() => router.push(`/courses/${course.slug}`)}>
+              Watch the course
+            </Button>
           </OwnedCourseCard>
         ))}
       </section>
