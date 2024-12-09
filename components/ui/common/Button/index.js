a7b2a713eae4ec9f6ca34916936849844e9d80ac
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-12-09 22:09:07 +0400

    style improvements

--- components/ui/common/Button/index.js
+++ components/ui/common/Button/index.js
@@ -1,5 +1,6 @@
 const Button = ({ children, className = "", variant = "purple", ...props }) => {
   const variants = {
+    white: "text-black bg-white",
     purple: "text-white bg-indigo-600 hover:bg-indigo-700",
     red: "text-white bg-red-600 hover:bg-red-700",
     lightPurple: "text-indigo-700 bg-indigo-100 hover:bg-indigo-200",
@@ -7,7 +8,7 @@ const Button = ({ children, className = "", variant = "purple", ...props }) => {
 
   return (
     <button
-      className={`disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none px-8 py-3 border text-base font-medium rounded-md ${variants[variant]} ${className}`}
+      className={`disabled:opacity-50 disabled:cursor-not-allowed xs:px-8 xs:py-3 border rounded-md text-base font-medium ${className} ${variants[variant]}`}
       {...props}
     >
       {children}
