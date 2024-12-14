73744800c38ee64de684a19d876496df96b23a24
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-12-14 16:02:05 +0400

    created ManagedCourseCard component

--- components/ui/Course/index.js
+++ components/ui/Course/index.js
@@ -4,4 +4,5 @@ export { default as Curriculum } from "./Curriculum";
 export { default as KeyPoints } from "./KeyPoints";
 export { default as CourseCard } from "./Card";
 export { default as OwnedCourseCard } from "./Card/OwnedCourseCard";
+export { default as ManagedCourseCard } from "./Card/ManagedCourseCard";
 export { default as CourseFilter } from "./Filter";
