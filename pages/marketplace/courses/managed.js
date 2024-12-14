6e7febdf8765e1fdcb72b9c8e403f5dc5e8ab40f
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-12-14 16:24:34 +0400

    itarated over courses and displayed in managed page

--- pages/marketplace/courses/managed.js
+++ pages/marketplace/courses/managed.js
@@ -1,6 +1,10 @@
 import { BaseLayout } from "@/components/ui/layout";
 import { MarketHeader } from "@/components/ui/Marketplace";
-import { CourseFilter, OwnedCourseCard } from "@/components/ui/Course";
+import {
+  CourseFilter,
+  OwnedCourseCard,
+  ManagedCourseCard,
+} from "@/components/ui/Course";
 import { Button } from "@/components/ui/common";
 
 import { useAccount, useManagedCourses } from "@/components/hooks/web3";
@@ -18,18 +22,20 @@ const ManagedCourses = () => {
         <CourseFilter />
       </div>
       <section className="grid grid-cols-1">
-        {/* <OwnedCourseCard>
-          <div className="flex mr-2 relative rounded-md">
-            <input
-              type="text"
-              name="account"
-              id="account"
-              className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
-              placeholder="0x2341ab..."
-            />
-            <Button>Verify</Button>
-          </div>
-        </OwnedCourseCard>{" "} */}
+        {managedCourses.data?.map((course) => (
+          <ManagedCourseCard key={course.ownedCourseId} course={course}>
+            <div className="flex mr-2 relative rounded-md">
+              <input
+                type="text"
+                name="account"
+                id="account"
+                className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
+                placeholder="0x2341ab..."
+              />
+              <Button>Verify</Button>
+            </div>
+          </ManagedCourseCard>
+        ))}
       </section>
     </BaseLayout>
   );
