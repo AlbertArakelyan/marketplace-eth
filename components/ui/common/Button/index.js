b8436b5858cbf0e23d7e2812408aa1b8f7d94725
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-12-15 13:14:08 +0400

    added activate course functionality on frontend

--- components/ui/common/Button/index.js
+++ components/ui/common/Button/index.js
@@ -4,6 +4,7 @@ const Button = ({ children, className = "", variant = "purple", ...props }) => {
     purple: "text-white bg-indigo-600 hover:bg-indigo-700",
     red: "text-white bg-red-600 hover:bg-red-700",
     lightPurple: "text-indigo-700 bg-indigo-100 hover:bg-indigo-200",
+    green: "text-white bg-green-600 hover:bg-green-700",
   };
 
   return (
--- pages/marketplace/courses/managed.js
+++ pages/marketplace/courses/managed.js
@@ -32,7 +32,7 @@ const VerificationInput = ({ onVerify }) => {
 };
 
 const ManagedCourses = () => {
-  const { web3 } = useWeb3();
+  const { web3, contract } = useWeb3();
   const { account } = useAdmin({ redirectTo: "/marketplace" });
   const { managedCourses } = useManagedCourses(account);
 
@@ -52,8 +52,18 @@ const ManagedCourses = () => {
     }
   };
 
+  const activateCourse = async (courseHash) => {
+    try {
+      await contract.methods.activateCourse(courseHash).send({
+        from: account.data,
+      });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   if (!account.isAdmin) {
-    return null;  
+    return null;
   }
 
   return (
@@ -78,6 +88,12 @@ const ManagedCourses = () => {
             {proofedOwnership[course.hash] === false && (
               <div className="mt-2">
                 <Message type="danger">Wrong Proof!</Message>
+              </div>
+            )}
+            {course.state === "purchased" && (
+              <div className="mt-2">
+                <Button variant="green" onClick={() => activateCourse(course.hash)}>Activate</Button>
+                <Button variant="red">Deactivate</Button>
               </div>
             )}
           </ManagedCourseCard>
